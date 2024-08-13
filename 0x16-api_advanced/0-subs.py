ef40390334b94aac17dfdd04a9ed49fe1448b950
Delali Funani <dfunani@live.co.za>
2024-08-13 08:38:31 -0700

    added removed try - rety

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Reddit API - Subscribers"""
-from requests import get
+import requests
 
 BASE_URL = "https://www.reddit.com/"
 HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Mobile Safari/537.36 Edg/127.0.0.0"
+    "User-Agent": "Mozilla/5.0 Linux 6.0 Edg/127.0.0.0"
 }
 
 def number_of_subscribers(subreddit):
@@ -15,7 +15,7 @@ def number_of_subscribers(subreddit):
     """
     endpoint = f"r/{subreddit}/about.json"
     url = f"{BASE_URL}{endpoint}"
-    response = get(url, headers=HEADERS, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
-    return response.json().get("data").get("subscribers")
+    response = requests.get(url, headers=HEADERS, allow_redirects=False)
+    if response.ok:
+        return response.json().get("data", {}).get("subscribers", 0)
+    return 0
