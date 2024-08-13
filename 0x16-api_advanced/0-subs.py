40e0dd61b94b36654c8ef7620395dc3e2a9d13b6
Delali Funani <dfunani@live.co.za>
2024-08-13 08:42:40 -0700

    addede remaining api calls

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-"""Reddit API - Subscribers"""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
-BASE_URL = "https://www.reddit.com/"
-HEADERS = {
-    "User-Agent": "Mozilla/5.0 Linux 6.0 Edg/127.0.0.0"
-}
 
 def number_of_subscribers(subreddit):
-    """Retrieves the Subscribers.
-
-    Args:
-        subreddit (str): sub-reddit aka reddit group.
-    """
-    endpoint = f"r/{subreddit}/about.json"
-    url = f"{BASE_URL}{endpoint}"
-    response = requests.get(url, headers=HEADERS, allow_redirects=False)
-    if response.ok:
-        return response.json().get("data", {}).get("subscribers", 0)
-    return 0
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
